f8a47613e6efbd428f4a39a8c5f502bde91d1f1b
foxching <rechielagria@gmail.com>
2023-11-21 07:54:47 +0800

    more routing lessons

--- Frontend/src/app/property/property-detail/property-detail.component.ts
+++ Frontend/src/app/property/property-detail/property-detail.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-property-detail',
   templateUrl: './property-detail.component.html',
-  styleUrls: ['./property-detail.component.css']
+  styleUrls: ['./property-detail.component.css'],
 })
 export class PropertyDetailComponent {
+  public propertyId: number = 0;
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.propertyId = +this.route.snapshot.params['id'];
+    this.route.params.subscribe((params)=> {
+      this.propertyId = +params['id']
+    })
+  }
+
+  selectNextPage() {
+    this.propertyId += 1;
+    this.router.navigate(['property-detail', this.propertyId])
+  }
 }

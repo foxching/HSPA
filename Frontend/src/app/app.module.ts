55547cfe7ebdbbdaccb78b4b19708e23aa1285ba
foxching <rechielagria@gmail.com>
2023-11-26 11:17:34 +0800

    template driven forms

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -27,7 +27,7 @@ const appRoutes: Routes = [
     AddPropertyComponent,
     PropertyDetailComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, HttpClientModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [HousingService],
   bootstrap: [AppComponent],
 })
--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
-  styleUrls: ['./add-property.component.css']
+  styleUrls: ['./add-property.component.css'],
 })
 export class AddPropertyComponent {
-  
-  constructor(private router: Router) {}
+  @ViewChild('Form', { static: false }) addPropertyForm: NgForm | undefined;
 
-  onBack(){
+  constructor(private router: Router) {
+  }
+
+  onBack() {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log('congrats', this.addPropertyForm);
   }
 }
